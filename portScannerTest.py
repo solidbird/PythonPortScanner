a8014ce8c1fcef33d9973bdc8917fa50a9c1b553
solidbird <43043269+solidbird@users.noreply.github.com>
2023-05-23 17:22:20 +0200

    Add files via upload

---
+++ portScannerTest.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 23 14:39:35 2023
+
+@author: Ich
+"""
+
+import os
+#import concurrent.futures
+
+import threading
+import click
+import socket
+
+port_list = []
+
+def open_sock(timeout: int) -> socket:
+    s = socket.socket()
+    s.settimeout(timeout)
+    
+    return s
+
+def scan_ports(host, portrange, timeout):
+        
+    for p in range(portrange[0],portrange[1]+1):
+        s = open_sock(timeout)
+        
+        try:
+            #print(f"{host}:{p}")
+            s.connect((host, p))
+        except:
+            #print(f"closed!")
+            continue
+        
+        port_list.append(p)
+
+@click.command()
+@click.option('--host', '-h', prompt='Host', help="Host the port Scanner should connect to.")
+@click.option('--ports', '-p', default="1-6000", help=f"'{os.path.basename(__file__)} -p 1-200' Port range for the port Scanner.")
+@click.option('--timeout', '-t', default=1, help="Timeout of each port connection.")
+def main(host, ports, timeout):
+    
+    port_start_end = ports.split('-')
+    port_start_end = (int(port_start_end[0]), int(port_start_end[1]))
+    
+    x=port_start_end[0]
+    y=port_start_end[1]
+    
+    thread_count = int((y-x)/2)
+    thread_list = []
+    
+    step = int((y-x)/thread_count)
+
+    for a in range(x,y+1,step):
+        end_bound = a+step-1
+        
+        if end_bound <= y:
+            #executor.map(scan_ports, host, (a,end_bound), timeout)
+            
+            #print(f"{thread_count} Thread Started!")
+            thr = threading.Thread(target=scan_ports, args=(host, (a,end_bound), timeout))
+            thread_list.append(thr)
+            thr.start()
+             
+    for x in thread_list:
+        x.join()
+    
+    for x in port_list:
+        print(f"Port open: {x}")
+
+if __name__ == "__main__":
+    main()
